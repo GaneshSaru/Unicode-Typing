cd02daa2ace200ed3cb5c6de1174fc89f93e0b25
Ganesh._.Saru <107201590+GaneshSaru@users.noreply.github.com>
2024-05-05 17:35:10 +0545

    Update guide.php

--- pages/guide.php
+++ pages/guide.php
@@ -66,7 +66,6 @@
     </header>
         <section class="first">
             <div class="info">
-                    <h4>Unicode Nepali Typing</h4>
                     <p>Unicode Nepali Typing, your go-to web platform for mastering Nepali typing using the traditional keyboard layout. Elevate your typing skills with our range of lessons and levels, designed to help you improve your accuracy and speed. Keep track of your progress easily with detailed reports. Start your journey towards becoming a proficient Nepali typist today!</p>
            </div> 
            <img src="../_images/clipart.jpg" alt="clipart">
@@ -101,47 +100,42 @@
                 <p>Correct errors as soon as they occur to prevent the formation of bad typing habits.</p>
                 <h5>6. Practice Regularly:</h5>
                 <p> Regular practice is key to improving typing speed and accuracy. Use online typing tutors or typing games to enhance your skills.</p>
-
-                <h4>Keyboard layout</h4>
+                   <h4>Keyboard layout</h4>
                 <img src="../_images/layout.jpg" alt="keyboard layout">
                 <img src="../_images/keyboardlayout Traditional.jpg" alt="keyboardlayout Traditional">
                 <img src="../files/Keys1.png" alt="keylayout1" style="height: 600px;">
                 <img src="../files/Keys2.png" alt="keylayout2">
             </div>
-        </section>
-        <div class="notes">
-            <h4>युनिकोड नेपाली टाईपिङ् प्रयोग गरी टाईपिङ् अभ्यास गर्दा ध्यान दिनुपर्ने कुराहरुः</h4>
-            <li>नेपाली अंकहरु लेख्न परेमा किबोर्ड माथिका Number Key सिधै प्रेस गर्नुहोस् ।</li>
-            <li>किबोर्डमा अवस्थित अक्षरहरु जस्तै ङ्ग, ह्य, क्क, त्त, ड्ढ, ट्ट आदी, सम्बन्धित किहरु प्रयोग गर्नुहोस् ।</li>
-            <li>यसै गरी अन्य अक्षर लेख्ने:</li>
-                <p>जस्तै : प्र, व्र, क्र बनाउन परेमा : पहिला आवत्श्यक अक्षर लेखेर \ बटन प्रेस गरी 'र' अर्थात / बटन प्रेस गर्नुहोस् ।</p> <br>
-                <p>जस्तै : प्रविन लेख्न परेमा : पहिला 'प' लेखि त्यसपछि \ बटन प्रेस गरी / बटन अर्थात 'र' अक्षर प्रेस गर्नुहोस् ।</p>
-            <li>आधा अक्षर लेख्न परेमा जस्तै : 'ध्यान'; पहिला 'ध' लेखेर \ बटन प्रेस गर्ने </li>
-            <li>'र्‍याल' लेख्न परेमा पहिला 'र्' बटन प्रेस गरी '=' बटन प्रेस गरी 'याल' लेख्नुहोस् ।</li>
-            <li>मात्रा:आकार (ा), इकार(ि),ईकार(ी), उकार(ु), ऊकार(ू), एकार(े), ‌‌ऐकार(ै), ओकार(ो), औकार(ौ), आदी लेख्न पहिला व्यंजन अक्षर लेखेर पछि मात्रा अक्षर लेख्ने </li>
-            <li>नेपाली युनिकोड ट्रेडिशनल किबोर्ड(Nepali Unicode Traditional Keyboard) मा अल्पविराम (comma ,) समावेश नभएको तर युनिकोड नेपाली टाईपिङ् प्रयोग गर्दा ',' कि नै प्रयोग गर्नुहोस् । </li>
-            <li>किबोर्डमा अवस्थित नभएका अक्षरहरु टाईपिङ् गर्ने उदाहरणहरुः</li>
-        </div>
-        <section class="fourth">
-            <div class="container">
-                <img src="../files/Example.jpg" alt="Examples" style="height: 1200px;">  
-            </div> 
-            <br>
-            <div class="file">
-              <a href="../files/keylayouts&hints.pdf" download="keylayouts&hints.pdf">Download above files.</a>
+            </section>
+        <section class="third">
+            <div class="notes">
+                <h4>युनिकोड नेपाली टाईपिङ् प्रयोग गरी टाईपिङ् अभ्यास गर्दा ध्यान दिनुपर्ने कुराहरुः</h4>
+                <li>नेपाली अंकहरु लेख्न परेमा किबोर्ड माथिका Number Key सिधै प्रेस गर्नुहोस् ।</li>
+                <li>किबोर्डमा अवस्थित अक्षरहरु जस्तै ङ्ग, ह्य, क्क, त्त, ड्ढ, ट्ट आदी, सम्बन्धित किहरु प्रयोग गर्नुहोस् ।</li>
+                <li>यसै गरी अन्य अक्षर लेख्ने:</li>
+                    <p>जस्तै : प्र, व्र, क्र बनाउन परेमा : पहिला आवत्श्यक अक्षर लेखेर \ बटन प्रेस गरी 'र' अर्थात / बटन प्रेस गर्नुहोस् ।</p> <br>
+                    <p>जस्तै : प्रविन लेख्न परेमा : पहिला 'प' लेखि त्यसपछि \ बटन प्रेस गरी / बटन अर्थात 'र' अक्षर प्रेस गर्नुहोस् ।</p>
+                <li>आधा अक्षर लेख्न परेमा जस्तै : 'ध्यान'; पहिला 'ध' लेखेर \ बटन प्रेस गर्ने </li>
+                <li>'र्‍याल' लेख्न परेमा पहिला 'र्' बटन प्रेस गरी '=' बटन प्रेस गरी 'याल' लेख्नुहोस् ।</li>
+                <li>मात्रा:आकार (ा), इकार(ि),ईकार(ी), उकार(ु), ऊकार(ू), एकार(े), ‌‌ऐकार(ै), ओकार(ो), औकार(ौ), आदी लेख्न पहिला व्यंजन अक्षर लेखेर पछि मात्रा अक्षर लेख्ने </li>
+                <li>नेपाली युनिकोड ट्रेडिशनल किबोर्ड(Nepali Unicode Traditional Keyboard) मा अल्पविराम (comma ,) समावेश नभएको तर युनिकोड नेपाली टाईपिङ् प्रयोग गर्दा ',' कि नै प्रयोग गर्नुहोस् । </li>
+                <li>किबोर्डमा अवस्थित नभएका अक्षरहरु टाईपिङ् गर्ने उदाहरणहरुः</li>
             </div>
         </section>
+       
+        <section class="forth">
+                <img src="../files/Example.jpg" alt="Examples" style="height: 1200px;">  
+                <a href="../files/keylayouts&hints.pdf" download="keylayouts&hints.pdf">Download above files.</a>
+        </section>
     <footer>
-        <div class="devby">
-        <h3>Unicode नेपाली Typing</h3>
+        <p class="p_title">Unicode नेपाली Typing</p>
         <p style="text-decoration: underline; text-underline-offset: 5px;">Developed By: </p>
         <p>
-            Ganesh Saru <br>
-            Himal Thapa <br>
-            Ramchandra Karki <br>
-            (BECE_2021)
+        Ganesh Saru <br>
+        Himal Thapa <br>
+        Ramchandra Karki <br>
+        (BECE_2021)
         </p>
-        </div>
     </footer>  
 </body>
 </html>
